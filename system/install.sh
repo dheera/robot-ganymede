628d2f16bb0bc371a9b7c04f344c7fdbe31aadf4
dheera <dheera@dheera.net>
2019-04-02 05:39:39 +0000

    updat

--- system/install.sh
+++ system/install.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-
-echo "generating ssh key ..."
-ssh-keygen -t rsa
-
-echo "installing apt packages ..."
-sudo apt-get -f install
 sudo apt-get update
-cat apt-installs.txt | xargs -n 1 sudo apt-get install -y
+cat apt-installs.txt | xargs sudo apt-get install -y
+cat pip-installs.txt | xargs sudo pip install --update
+cat pip-installs.txt | xargs sudo pip3 install --update
 
-echo "install pip packages (python2) ..."
-cat pip-install.txt | xargs -n 1 sudo pip install
-
-echo "install pip packages (python3) ..."
-cat pip-install.txt | xargs -n 1 sudo pip3 install
-
-echo "installing root files ..."
 sudo cp -rv root/* /
-
-sudo systemctl enable robot
 sudo systemctl enable autossh
+sudo systemctl enable robot
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    ssh-keygen -t rsa -N ''
+fi
